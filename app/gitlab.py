8912ede39f64cb29e1bc5b37e8ebbc5c69f423a8
Clément GUICHARD <clement.guichard@csgroup.eu>
2023-07-18 16:13:25 +0200

    feat: api collect project with keyset pagination

--- app/gitlab.py
+++ app/gitlab.py
@@ -2,6 +2,7 @@ import asyncio
 from typing import Any, TypedDict
 from urllib import parse
 
+import aiohttp
 from fastapi import HTTPException
 
 from .utils import AiohttpClient, get_markdown_metadata
@@ -23,7 +24,9 @@ class GitlabClient:
         self.token = token
 
     async def get_projects(self, topic_name: str) -> list[GitlabProjectInfo]:
-        return await self._request(f"/projects?topic={topic_name}&simple=true")
+        return await self._request(
+            f"/projects?topic={topic_name}&simple=true", collect=True
+        )
 
     async def get_project_metadata(
         self, project_path: str
@@ -47,19 +50,65 @@ class GitlabClient:
         )
 
     async def _request(
-        self, endpoint: str, media_type="json"
+        self, endpoint: str, media_type="json", collect: bool = False
     ) -> dict[str, Any] | list[Any] | str:
         async with AiohttpClient() as client:
-            req = await client.get(
-                f"{self.api_url}{endpoint}", headers={"PRIVATE-TOKEN": self.token}
-            )
-            if req.ok:
-                match media_type:
-                    case "text":
-                        return await req.text()
-                    case "json" | _:
-                        return await req.json()
+            link = f"{self.api_url}{endpoint}"
+            if collect:
+                params = {
+                    "per_page": 20,
+                    "pagination": "keyset",
+                    "order_by": "id",
+                    "sort": "asc",
+                }
+                url_parts = list(parse.urlparse(link))
+                url_parts[4] = parse.urlencode(
+                    dict(parse.parse_qsl(url_parts[4])) | params
+                )
+                link = parse.urlunparse(url_parts)
+            async with client.get(link, headers={"PRIVATE-TOKEN": self.token}) as resp:
+                if resp.ok:
+                    match media_type:
+                        case "text":
+                            return await resp.text()
+                        case "json" | _:
+                            if collect:
+                                return await self._collect(client, resp)
+                            return await resp.json()
+                raise HTTPException(
+                    status_code=resp.status,
+                    detail=f"{resp.url}: {await resp.text()}",
+                )
+
+    async def _collect(
+        self, client: aiohttp.ClientSession, response: aiohttp.ClientResponse
+    ) -> list[Any]:
+        items = await response.json()
+
+        if not isinstance(items, list):
             raise HTTPException(
-                status_code=req.status,
-                detail=f"{req.url}: {await req.text()}",
+                status_code=422, detail="Unexpected: requested API do not return a list"
             )
+
+        next_link = self._get_next_link(response)
+        while next_link:
+            async with client.get(
+                next_link, headers={"PRIVATE-TOKEN": self.token}
+            ) as resp:
+                if resp.ok:
+                    items.extend(await resp.json())
+                    next_link = self._get_next_link(resp)
+                else:
+                    raise HTTPException(status_code=resp.status)
+                next_link = None
+
+        return items
+
+    def _get_next_link(self, response: aiohttp.ClientResponse) -> str | None:
+        if next_link_header := response.headers.get("Link"):
+            next_link, rel, *_ = next_link_header.split(";")
+            next_link = next_link.strip("<>")
+            rel = rel.strip().removeprefix("rel=").strip('"')
+            if rel == "next":
+                return next_link
+        return None
