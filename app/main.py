b177d32e91c60dd8981424637e24f68a697de0e7
Clément GUICHARD <clement.guichard@csgroup.eu>
2023-08-09 14:10:14 +0200

    chore: format with black

--- app/main.py
+++ app/main.py
@@ -44,8 +44,10 @@ app.add_middleware(
 )
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 
+
 @app.get("/")
 async def status():
     return [{"status": "ok"}]
+
 
 app.include_router(router)
