5c14c3c74933f991d868d55b6341bdd23599c11a
Clément GUICHARD <clement.guichard@csgroup.eu>
2023-11-14 10:33:24 +0100

    feat: add resolve service to return project id from project path

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -8,6 +8,7 @@ from app.utils.http import url_for
 from .auth import router as auth_router
 from .download import router as download_router
 from .proxy import router as proxy_router
+from .resolve import router as resolve_router
 from .stac import router as stac_router
 
 router = APIRouter(prefix="/{gitlab}")
@@ -31,5 +32,6 @@ async def views_index(
 
 router.include_router(auth_router, prefix="/auth", tags=["auth"])
 router.include_router(stac_router, prefix="/stac", tags=["stac"])
+router.include_router(resolve_router, prefix="/resolve", tags=["stac"])
 router.include_router(download_router, prefix="/download", tags=["download"])
 router.include_router(proxy_router, prefix="/api", tags=["proxy"])
