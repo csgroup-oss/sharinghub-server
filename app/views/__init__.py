462409a5daaeeb25eaf702278852ced8ed34d4b9
Clément GUICHARD <clement.guichard@csgroup.eu>
2023-12-06 13:30:36 +0100

    feat: add config endpoint with topics localization

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,6 +1,7 @@
 from fastapi.routing import APIRouter
 
 from .auth import router as auth_router
+from .configuration import router as configuration_router
 from .download import router as download_router
 from .proxy import router as proxy_router
 from .stac import router as stac_router
@@ -8,6 +9,7 @@ from .stac import router as stac_router
 router = APIRouter()
 
 router.include_router(auth_router, prefix="/auth", tags=["auth"])
+router.include_router(configuration_router, prefix="/config", tags=["configuration"])
 router.include_router(stac_router, prefix="/stac", tags=["stac"])
 router.include_router(download_router, prefix="/download", tags=["download"])
 router.include_router(proxy_router, prefix="/api", tags=["proxy"])
---
+++ app/views/configuration.py
@@ -0,0 +1,24 @@
+from fastapi.routing import APIRouter
+
+from app.config import STAC_CATALOGS_TOPICS
+
+router = APIRouter()
+
+
+@router.get("/")
+async def configuration():
+    text_keys = ["title", "description"]
+    return {
+        "topics": {
+            topic_name: {
+                "locales": {
+                    "en": {k: v for k, v in topic.items() if k in text_keys},
+                    **{
+                        locale: {k: v for k, v in translation.items() if k in text_keys}
+                        for locale, translation in topic.get("locales", {}).items()
+                    },
+                }
+            }
+            for topic_name, topic in STAC_CATALOGS_TOPICS.items()
+        },
+    }
