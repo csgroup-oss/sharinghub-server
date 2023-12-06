b6323d8db06dfa987a1981dc53ab25cb4b20d247
Clément GUICHARD <clement.guichard@csgroup.eu>
2023-12-06 15:10:35 +0100

    fix: optional oauth config

--- app/config.py
+++ app/config.py
@@ -91,11 +91,14 @@ ENABLE_CACHE: bool = conf(
 #### GitLab ####
 
 GITLAB_URL: str = conf("gitlab.url", "GITLAB_URL", cast=str)
-_CLIENT_ID: str = conf("gitlab.oauth.client-id", "GITLAB_OAUTH_CLIENT_ID", cast=str)
-_CLIENT_SECRET: str = conf(
+_CLIENT_ID: str | None = conf(
+    "gitlab.oauth.client-id", "GITLAB_OAUTH_CLIENT_ID", cast=str
+)
+_CLIENT_SECRET: str | None = conf(
     "gitlab.oauth.client-secret", "GITLAB_OAUTH_CLIENT_SECRET", cast=str
 )
 GITLAB_OAUTH = {"client_id": _CLIENT_ID, "client_secret": _CLIENT_SECRET}
+GITLAB_OAUTH = {k: v for k, v in GITLAB_OAUTH.items() if v}
 
 ####  STAC  ####
 
