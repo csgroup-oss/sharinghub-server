142f855181d802998928fc402619803b6a89c3cc
Clément GUICHARD <clement.guichard@csgroup.eu>
2023-12-21 11:42:35 +0100

    refactor: improve config generation at endpoint

--- app/views/configuration.py
+++ app/views/configuration.py
@@ -13,6 +13,7 @@ router = APIRouter()
 @router.get("/")
 async def configuration():
     text_keys = ["title", "description"]
+    exclude_keys = ["locales"]
     return {
         "jupyterlab": {
             "url": JUPYTERLAB_URL,
@@ -22,35 +23,36 @@ async def configuration():
                 "default-token": GITLAB_OAUTH_DEFAULT_TOKEN,
             }
         },
-        "topics": {
-            topic_name: {
-                "locales": {
-                    "en": {k: v for k, v in topic.items() if k in text_keys},
-                    **{
-                        locale: {k: v for k, v in translation.items() if k in text_keys}
-                        for locale, translation in topic.get("locales", {}).items()
-                    },
-                },
-                "logo": topic.get("logo", {}),
-            }
-            for topic_name, topic in STAC_CATALOGS_TOPICS.items()
-        },
         "root": {
             **{
                 k: v
                 for k, v in STAC_ROOT_CONF.items()
-                if k not in ["locales", "description"]
+                if k not in ["id", *text_keys, *exclude_keys]
             },
             "locales": {
-                "en": {
-                    k: v
-                    for k, v in STAC_ROOT_CONF.items()
-                    if k not in ["locales", "title", "id"]
-                },
+                "en": {k: v for k, v in STAC_ROOT_CONF.items() if k in text_keys},
                 **{
                     locale: {k: v for k, v in translation.items()}
                     for locale, translation in STAC_ROOT_CONF.get("locales", {}).items()
                 },
             },
+        },
+        "topics": {
+            topic_name: {
+                **{
+                    k: v
+                    for k, v in topic.items()
+                    if k not in ["features", *text_keys, *exclude_keys]
+                },
+                "locales": {
+                    "en": {k: v for k, v in topic.items() if k in text_keys},
+                    **{
+                        locale: {k: v for k, v in translation.items()}
+                        for locale, translation in topic.get("locales", {}).items()
+                    },
+                },
+                # "logo": topic.get("logo", {}),
+            }
+            for topic_name, topic in STAC_CATALOGS_TOPICS.items()
         },
     }
