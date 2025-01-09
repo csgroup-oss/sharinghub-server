637c81d8fb658550502bec13c3b2f5330fefd79f
Clément GUICHARD <clement.guichard@csgroup.eu>
2025-01-09 15:01:21 +0100

    chore: update copyright headers

--- scripts/copyright.py
+++ scripts/copyright.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2024, CS GROUP - France, https://www.csgroup.eu/
+# Copyright 2025, CS GROUP - France, https://www.csgroup.eu/
 #
 # This file is part of SharingHub project
 #
